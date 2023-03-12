9e98700b342e8d1d079481bdfea518c7acfb1055
Halimiyau Yusuf <yusufhalimu@gmail.com>
2023-03-12 07:42:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var name = document.getElementById("name");
+var button = document.getElementById("button");
+button.addEventListener("click", function(){
+    var name = document.getElementById("name");
+    var message = name.value;
+    alert("Thank you "+message+ ", you have successfully contacted us and will be responded to in due time");
+    name.value="";
+})
+
+var img=document.getElementById('img');
+var slides = ["images/images1.jpg", "images/images2.jpg", "images/images3.jpg"];
+var start = 0;
+
+function slider(){
+    if(start<slides.length){
+        start=start+1;
+    }
+    else{
+        start=1;
+    }
+    console.log(img);
+    img.innerHTML = "<img src="+slides[start-1]+">";
+
+}
+setinterval(slider, 2000);
+
