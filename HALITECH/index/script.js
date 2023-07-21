128cbc741c2edc49a96be929846e7121a023f40f
Halimiyau Yusuf <yusufhalimu@gmail.com>
2023-07-21 22:59:27 +0100

    Add files via upload

--- HALITECH/index/script.js
+++ HALITECH/index/script.js
@@ -1,11 +1,11 @@
-var name = document.getElementById("name");
-var button = document.getElementById("button");
-button.addEventListener("click", function(){
-    var name = document.getElementById("name");
-    var message = name.value;
-    alert("Thank you "+message+ ", you have successfully contacted us and will be responded to in due time");
-    name.value="";
-})
+// var name = document.getElementById("name");
+// var button = document.getElementById("button");
+// button.addEventListener("click", function(){
+//     var name = document.getElementById("name");
+//     var message = name.value;
+//     alert("Thank you "+message+ ", you have successfully contacted us and will be responded to in due time");
+//     name.value="";
+// })
 
 var img=document.getElementById('img');
 var slides = ["images/images1.jpg", "images/images2.jpg", "images/images3.jpg"];
